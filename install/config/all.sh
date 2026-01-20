9b930cc634d2214b726d1661f743c2fc666dd35a
Drew Hutton (Yoroshi) <github@yoro.link>
2026-01-20 10:39:40 +0000

    chore: remove powerprofilesctl

--- install/config/all.sh
+++ install/config/all.sh
@@ -10,7 +10,7 @@ run_logged $OMARCHY_INSTALL/config/ssh-flakiness.sh
 run_logged $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
 run_logged $OMARCHY_INSTALL/config/xcompose.sh
 run_logged $OMARCHY_INSTALL/config/mise-work.sh
-run_logged $OMARCHY_INSTALL/config/fix-powerprofilesctl-shebang.sh
+# run_logged $OMARCHY_INSTALL/config/fix-powerprofilesctl-shebang.sh
 run_logged $OMARCHY_INSTALL/config/docker.sh
 run_logged $OMARCHY_INSTALL/config/mimetypes.sh
 run_logged $OMARCHY_INSTALL/config/localdb.sh
