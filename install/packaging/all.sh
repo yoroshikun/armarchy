9450996104f914b5ab25eeaf0e753937c111eb62
Drew Hutton (Yoroshi) <github@yoro.link>
2026-01-20 10:32:10 +0000

    chore: fix nvim followup

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -2,7 +2,7 @@ run_logged $OMARCHY_INSTALL/packaging/yay.sh    # Temporary, remove when ARM sup
 run_logged $OMARCHY_INSTALL/packaging/base.sh
 run_logged $OMARCHY_INSTALL/packaging/arch.sh
 run_logged $OMARCHY_INSTALL/packaging/fonts.sh
-run_logged $OMARCHY_INSTALL/packaging/nvim.sh
+# run_logged $OMARCHY_INSTALL/packaging/nvim.sh
 run_logged $OMARCHY_INSTALL/packaging/icons.sh
 run_logged $OMARCHY_INSTALL/packaging/webapps.sh
 run_logged $OMARCHY_INSTALL/packaging/tuis.sh
